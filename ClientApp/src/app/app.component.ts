7deafe231cc621f7ddd0902e603a3a8c2897d771
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2020-08-31 16:01:46 +0300

    remove title variable

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Movie Mash';
-
   
-  
-
   constructor() {
 
   }
